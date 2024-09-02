3ffc7c5558f3abb077fc50679ee4e7a66d6938bd
Molina0127 <guimoltri@gmail.com>
2024-09-01 23:55:47 -0300

    Try to solve exercise

---
+++ Strings/Exercises/replaceVowelByAsterisk.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int reprocessing() {
+    int op;
+    do {
+        printf("Deseja contar o numero de vogais de outra frase? (1 - sim/2 - nao)\n");
+        scanf("%d", &op);
+        getchar();
+    } while(op != 1 && op != 2);
+}
+
+int verifyVowel(char letter) {
+    letter = toupper(letter);
+    if (letter == 'A' || letter == 'E' || letter == 'I' || letter == 'O' || letter == 'U') {
+        return 1;
+    }
+    else {
+        return 0;
+    }
+}
+
+char replaceVowelByAsterisk() {
+    if (verifyVowel(sentence[i])) {
+        sentence[i] = '*';
+    }
+}
+
+char countVowel() {
+    char sentence[100];
+    int cont = 0;
+    printf("Escreva uma frase: ");
+    fgets(sentence, 100, stdin);
+    fflush(stdin);
+    
+    for(int i = 0; sentence[i]; i++) {
+       replaceVowelByAsterisk();
+    }
+
+    printf("%s", sentence);
+    // return cont;
+}
+
+int main() {
+    int cont;
+    do {
+        //cont = countVowel();
+        //printf("O numero de vogais na frase eh: %d\n", cont);
+        countVowel();
+    } while(reprocessing() == 1);
+
+    return 0;
+}
