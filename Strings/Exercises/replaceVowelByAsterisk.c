041a8d0f3811fd921dde9d67e6f285f1634212aa
Molina0127 <guimoltri@gmail.com>
2024-09-02 06:07:47 -0300

    Add some validation layer

--- Strings/Exercises/replaceVowelByAsterisk.c
+++ Strings/Exercises/replaceVowelByAsterisk.c
@@ -2,13 +2,28 @@
 #include <string.h>
 #include <ctype.h>
 
-int reprocessing() {
-    int op;
+void clearInputBuffer() {
+    int ch;
+    while((ch = getchar()) != '\n' && ch != EOF);
+}
+
+int shouldRepeat() {
+    int userOption;
+    char input[10];
     do {
         printf("Deseja substituir as vogais de uma outra frase por '*'? (1 - sim/2 - nao)\n");
-        scanf("%d", &op);
-        getchar();
-    } while(op != 1 && op != 2);
+        fgets(input, 10, stdin);
+        sscanf(input, "%d", &userOption);
+        if ((userOption == 1) || (userOption == 2)) { 
+            break;
+        }
+        else {
+            clearInputBuffer();
+            printf("Entrada invalida. Por favor, digite 1 ou 2.\n");
+        }
+    } while(userOption != 1 && userOption != 2);
+    
+    return userOption;
 }
 
 int verifyVowel(char letter) {
@@ -21,24 +36,24 @@ int verifyVowel(char letter) {
     }
 }
 
-char replaceVowelByAsterisk(char sentence[100]) {
+char replaceVowelByAsterisk(char inputPhrase[100]) {
     printf("Escreva uma frase: ");
-    fgets(sentence, 100, stdin);
+    fgets(inputPhrase, 100, stdin);
     fflush(stdin);
     
-    for(int i = 0; sentence[i]; i++) {
-       if (verifyVowel(sentence[i])) {
-            sentence[i] = '*';
+    for(int i = 0; inputPhrase[i]; i++) {
+       if (verifyVowel(inputPhrase[i])) {
+            inputPhrase[i] = '*';
        }
     }
 }
 
 int main() {
-    char sentence[100];
+    char inputPhrase[100];
     do {
-        replaceVowelByAsterisk(sentence);
-        printf("%s", sentence);
-    } while(reprocessing() == 1);
+        replaceVowelByAsterisk(inputPhrase);
+        printf("%s", inputPhrase);
+    } while(shouldRepeat() == 1);
 
     return 0;
 }
