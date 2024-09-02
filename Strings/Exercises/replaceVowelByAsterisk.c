f2167072ac0be6150079e8810155d2fca5c2210b
Molina0127 <guimoltri@gmail.com>
2024-09-02 06:10:21 -0300

    Optimize validation layer

--- Strings/Exercises/replaceVowelByAsterisk.c
+++ Strings/Exercises/replaceVowelByAsterisk.c
@@ -10,20 +10,32 @@ void clearInputBuffer() {
 int shouldRepeat() {
     int userOption;
     char input[10];
-    do {
-        printf("Deseja substituir as vogais de uma outra frase por '*'? (1 - sim/2 - nao)\n");
-        fgets(input, 10, stdin);
-        sscanf(input, "%d", &userOption);
-        if ((userOption == 1) || (userOption == 2)) { 
-            break;
+    while(1) {
+        printf("\nDeseja verificar o numero de vogais em alguma outra frase? (1 - sim/2 - nao)\n");
+        fgets(input, sizeof(input), stdin);
+        
+        // Remove new line char
+        input[strcspn(input, "\n")] = 0;
+
+        if (strlen(input) > 1) {
+            printf("Entrada invalida. Por favor, digite 1 ou 2.\n");
+            clearInputBuffer();
+            continue;
+        }
+
+        if(sscanf(input, "%d", &userOption) == 1) {    
+            if ((userOption == 1) || (userOption == 2)) { 
+                return userOption;
+            }
+            else {
+                printf("Entrada invalida. Por favor, digite 1 ou 2.\n");
+            }
         }
         else {
-            clearInputBuffer();
             printf("Entrada invalida. Por favor, digite 1 ou 2.\n");
         }
-    } while(userOption != 1 && userOption != 2);
-    
-    return userOption;
+
+    }
 }
 
 int verifyVowel(char letter) {
