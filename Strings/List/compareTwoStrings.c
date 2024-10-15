98326223d942f2756b87b9f3abfe9f04fa916c25
Molina0127 <guimoltri@gmail.com>
2024-10-15 15:41:48 -0300

    Add compareTwoString function

--- Strings/List/compareTwoStrings.c
+++ Strings/List/compareTwoStrings.c
@@ -1,8 +1,29 @@
 #include <stdio.h>
+#include <string.h>
 
-char compareTwoStrings(char *str1, char *str2) {
-    for (int i = 0; str1[i]; i++);
-    for (int i = 0; str2[i]; i++);
+int compareTwoStrings(char *str1, char *str2) {
+    int comparison = 0;
+    for (int i = 0; i < 50 && comparison == 0; i++) {
+        if (str1[i] > str2[i]) {
+            comparison = 1;
+        }
+        else if (str1[i] < str2[i]) {
+            comparison = -1;
+        }
+    }
+    return comparison;
+}
+
+char treatSentence(char *sentence) {
+    char *p;
+    if(p=strchr(sentence, '\n')) {
+        *p = 0;
+    }
+    else {
+        scanf("%*[^\n]");
+        scanf("%*c"); // clear buffer upto newline
+    }
+
 }
 
 int main() {
@@ -10,12 +31,18 @@ int main() {
     char sentence2[50];
 
     printf("Digite uma frase: ");
-    fgets(sentence1, sizeof(sentence1), stdin);
+    fgets(sentence1, 50, stdin);
+    
+    treatSentence(sentence1);
 
     printf("Digite uma segunda frase: ");
-    fgets(sentence2, sizeof(sentence2), stdin);
+    fgets(sentence2, 50, stdin);
 
-    compareTwoStrings(sentence1, sentence2);
+    treatSentence(sentence2);
+
+    int comparison = compareTwoStrings(sentence1, sentence2);
+
+    printf("O resultado da comparacao eh igual a %d", comparison);
 
     return 0;
 }
