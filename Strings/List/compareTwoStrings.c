2c264b5cc7e22203015dfaad66710f9aefe6812b
Molina0127 <guimoltri@gmail.com>
2024-10-15 15:45:58 -0300

    Treating the final details in program

--- Strings/List/compareTwoStrings.c
+++ Strings/List/compareTwoStrings.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <string.h>
-
+/* String Manipulation - List 1 - Exercise 3 */
 int compareTwoStrings(char *str1, char *str2) {
     int comparison = 0;
     for (int i = 0; i < 50 && comparison == 0; i++) {
@@ -27,15 +27,15 @@ char treatSentence(char *sentence) {
 }
 
 int main() {
-    char sentence1[50];
-    char sentence2[50];
+    char sentence1[51];
+    char sentence2[51];
 
-    printf("Digite uma frase: ");
+    printf("Digite uma frase (MAX 50 CARACTERES): ");
     fgets(sentence1, 50, stdin);
     
     treatSentence(sentence1);
 
-    printf("Digite uma segunda frase: ");
+    printf("Digite uma segunda frase (MAX 50 CARACTERES): ");
     fgets(sentence2, 50, stdin);
 
     treatSentence(sentence2);
