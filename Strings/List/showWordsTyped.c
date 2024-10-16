7a10216cb11a38aed9b2fa8c1cb6983bafd02bb9
Molina0127 <guimoltri@gmail.com>
2024-10-16 14:40:01 -0300

    Add a solution to problem of show the words which were typed by user

---
+++ Strings/List/showWordsTyped.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <string.h>
+/* String Manipulation - List 1 - Exercise 14 */
+void treatSentence(char *sentence) {
+    char *p;
+    if (p=(strchr(sentence, '\n'))) {
+        *p = 0;
+    }
+    else {
+        scanf("%[^\n]");
+        scanf("%*c");
+    }
+}
+
+void showWordsTyped(char *sentence) {
+    int length1 = strlen(sentence);
+    int j;
+        
+    for (j = 0; j < length1; j++) {
+        if (sentence[j] != ' ') {
+            printf("%c", sentence[j]);
+        }
+        else if (sentence[j] == ' ') {
+            printf("\n");
+        }
+    }
+}
+
+int main() {
+    char sentence[50];
+    char word[30];
+    
+    printf("Digite uma frase: ");
+    fgets(sentence, 50, stdin);
+    
+    treatSentence(sentence);
+    
+    printf("\nAs palavras digitadas nessa frase sao:\n\n");
+    showWordsTyped(sentence);
+    
+
+    return 0;
+}
