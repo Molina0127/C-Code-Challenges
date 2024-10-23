d1f6bdce029e52b52562631c8467adea55c35a67
Molina0127 <guimoltri@gmail.com>
2024-10-22 23:46:53 -0300

    Add Solution to problem Verify if Point it is in Rectangle

---
+++ Structs/List/representingAPoint.c
@@ -0,0 +1,93 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#define MAX 50
+/* Structs - List 2 - Exercise 2 */
+struct point {
+    int x;
+    int y;
+};
+
+void clearBuffer() {
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF);
+}
+
+void treatSentence(char *sentence) {
+    char *p;
+    if(p = strchr(sentence, '\n')) {
+        *p = 0;
+    }
+}
+
+void toUpper(char *str) {
+    for (int i = 0; str[i] != '\0'; i++) {
+        str[i] = toupper(str[i]);
+    }
+}
+
+void verifyInRectangle(struct point v1, struct point v2, struct point p) {
+    struct point v3, v4;
+
+    v3.x = v1.x;
+    v3.y = v2.y;
+    v4.x = v2.x;
+    v4.y = v1.y;
+
+    if (p.x <= v2.x && p.x >= v1.x && p.y <= v2.y && p.y >= v1.y) {
+        printf("O ponto (%d, %d) esta dentro do retangulo", p.x, p.y);
+    }
+    else {
+        printf("O ponto (%d, %d) nao esta dentro do retangulo '-'", p.x, p.y);
+    }
+
+}
+
+int main() {
+    struct point v1, v2, p;
+    
+    printf("Vamos primeiramente definir os limites do retangulo\n");
+    printf("Coordenadas do vertice1 (Canto inferior esquerdo)\n");
+    printf("Digite o x do vertice1:\n");
+    scanf("%d", &v1.x);
+    printf("Digite o y do vertice1:\n");
+    scanf("%d", &v1.y);
+    printf("Coordenadas do vertice2 (Canto superior direito)\n");
+    printf("Digite o x do vertice2:\n");
+    scanf("%d", &v2.x);
+    printf("Digite o y do vertice2:\n");
+    scanf("%d", &v2.y);
+
+    printf("Agora digite as coordenadas do ponto\n");
+    printf("Digite o x do ponto:\n");
+    scanf("%d", &p.x);
+    printf("Digite o y do vertice1:\n");
+    scanf("%d", &p.y);
+
+    verifyInRectangle(v1, v2, p);
+
+    // printf("Escolha uma opcao (Digite o numero correspondente para acessa-la)\n");
+    // printf("1 - Mostrar todas as bandas\n2 - Pesquisar banda por Ranking\n3 - Listar Bandas por estilo musical\n4 - Verificar se a banda esta entre as favoritas\n");
+    // scanf("%d", &op);
+    // clearBuffer();
+
+    // switch (op) {
+    //     case 1:
+    //         showAllBands(bandsNumber, bands);
+    //         break;
+    //     case 2:
+    //         searchBand(bandsNumber, bands);
+    //         break;
+    //     case 3:
+    //         listBandsByMusicalStyle(bandsNumber, bands);
+    //         break;
+    //     case 4:
+    //         verifyBandInRanking(bandsNumber, bands);
+    //         break;
+    //     default:
+    //         printf("Opcao invalida '-'");
+    //         break;
+    // }
+
+    return 0;
+}
