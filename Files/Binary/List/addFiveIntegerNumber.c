c465abdbbb93d92ecca87eec16ab98d31632d1a7
Molina0127 <guimoltri@gmail.com>
2024-10-26 23:56:38 -0300

    add: initial commit, binary file

---
+++ Files/Binary/List/addFiveIntegerNumber.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/* Binary File - List 3 - Exercise 1 & 2 */
+
+// create dados.bin
+void addIntegerNumber() {
+    for (int i = 0; i < 5; i++) {
+
+    }
+}
+
+
+int main(){
+    return 0;
+}
